0b2328177dac4c2fe7eb2895724e48082cea6898
theSniper20 <montassarchaabani.freedom@gmail.com>
2024-12-09 04:04:10 +0100

    fix call service

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
-      // Removed refresh for production
-    }),
-  ],
-  build: {
-    // Example of production optimizations
-    outDir: 'public/build', // Ensure this points to the correct output directory
-    manifest: true, // Generate the manifest file for Laravel to find the assets
-    rollupOptions: {
-      // Additional optimizations or code-splitting can go here
-    },
-  },
+    plugins: [
+        viteCommonjs(),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+        }),
+    ],
+
 });
