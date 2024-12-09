0d913ef725da5112fdb018048e334087876c45a9
theSniper20 <montassarchaabani.freedom@gmail.com>
2024-12-09 03:54:58 +0100

    fix call service

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig({
-server: {
-    host: 'localhost',
-    port: 5173,
-  },
-    plugins: [
-        viteCommonjs(),
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    optimizeDeps: {
-        exclude: ["@cornerstonejs/dicom-image-loader"],
-        include: ["dicom-parser"],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      // Removed refresh for production
+    }),
+  ],
+  build: {
+    // Example of production optimizations
+    outDir: 'public/build', // Ensure this points to the correct output directory
+    manifest: true, // Generate the manifest file for Laravel to find the assets
+    rollupOptions: {
+      // Additional optimizations or code-splitting can go here
     },
+  },
 });
